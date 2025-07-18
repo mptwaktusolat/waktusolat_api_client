9ff89c57875411ca752f659cf90797c4db0a12cb
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-07-18 08:02:30 +0800

    ✨ Export all models

--- lib/waktusolat_api_client.dart
+++ lib/waktusolat_api_client.dart
@@ -4,9 +4,15 @@
 library;
 
 export 'src/waktusolat_api_client_base.dart';
-export 'src/models/mpt_waktu_solat_v2.dart';
-export 'src/models/mpt_prayer.dart';
-export 'src/models/hijri_date.dart';
 export 'src/extensions/prayer_time_extension.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+// Models
+export 'src/models/hijri_date.dart';
+export 'src/models/mpt_prayer_time.dart';
+export 'src/models/mpt_prayer.dart';
+export 'src/models/mpt_solat_v1_day.dart';
+export 'src/models/mpt_solat_v1_month.dart';
+export 'src/models/mpt_waktu_solat_v2.dart';
+export 'src/models/mpt_zone_by_gps.dart';
+export 'src/models/mpt_zone.dart';
+export 'src/models/mpt_zones.dart';
